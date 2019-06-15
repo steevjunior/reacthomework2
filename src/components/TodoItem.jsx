4f7c26c486165344454f24173a447bd3c2ecdb53
unknown <bitomagira@gmail.com>
2019-06-15 14:29:19 +0200

    first commit

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,90 +1,83 @@
 import React from "react";
-
+import "./todo.css";
 export class TodoItem extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            editing: false,
-            title: props.title
-        };
-    }
-
-    setEditMode = () => {
-        this.setState({
-            editing: true
-        });
+  constructor(props) {
+    super(props);
+    this.state = {
+      editing: false,
+      title: props.title
     };
+  }
 
-    onChange = e => {
-        let title = this.state.title;
-        title = e.target.value;
-        this.setState({ title });
-    };
+  setEditMode = () => {
+    this.setState({
+      editing: true
+    });
+  };
 
-    onClickSave = () => {
-        this.props.handleEdit(this.props.id, this.state.title);
-        this.setState({
-            editing: false
-        });
-    };
+  onChange = e => {
+    let title = this.state.title;
+    title = e.target.value;
+    this.setState({ title });
+  };
 
-    renderItem(completed, id, title, handleChecked, handleDelete) {
-        return (
-            <li
-                style={{
-                    color: completed ? "red" : "black",
-                    cursor: "pointer",
-                    display: "flex",
-                    justifyContent: "space-between",
-                    width: 300,
-                    marginBottom: "1vh"
-                }}
-                onDoubleClick={() => this.setEditMode()}
-            >
-                <div onClick={() => handleChecked(id)}>{title}</div>
-                <button onClick={() => handleDelete(id)}>x</button>
-            </li>
-        );
-    }
+  onClickSave = () => {
+    this.props.handleEdit(this.props.id, this.state.title);
+    this.setState({
+      editing: false
+    });
+  };
 
-    renderEdit(completed) {
-        return (
-            <li
-                style={{
-                    color: completed ? "red" : "black",
-                    cursor: "pointer",
-                    display: "flex",
-                    justifyContent: "space-between",
-                    width: 300,
-                    marginBottom: "1vh"
-                }}
-            >
-                <input
-                    value={this.state.title}
-                    onChange={this.onChange}
-                />
-                <button onClick={this.onClickSave}>save</button>
-            </li>
-        );
-    }
+  renderItem(completed, id, title, handleChecked, handleDelete) {
+    return (
+      <li
+        className="itemlist"
+        style={{
+          backgroundColor: completed ? "green" : "moccasin",
+          color: completed ? "white" : "black",
+          cursor: "pointer",
+          display: "flex",
+          justifyContent: "space-between",
+          width: 300,
+          marginBottom: "1vh"
+        }}
+        onDoubleClick={() => this.setEditMode()}
+      >
+        <div onClick={() => handleChecked(id)}>{title}</div>
+        <button onClick={() => handleDelete(id)}>x</button>
+      </li>
+    );
+  }
 
-    render() {
-        const {
-            completed,
-            id,
-            title,
-            handleChecked,
-            handleDelete,
-            handleEdit
-        } = this.props;
-        return this.state.editing
-            ? this.renderEdit(completed, id, handleEdit)
-            : this.renderItem(
-                  completed,
-                  id,
-                  title,
-                  handleChecked,
-                  handleDelete
-              );
-    }
+  renderEdit(completed) {
+    return (
+      <li
+        style={{
+          color: completed ? "red" : "black",
+          cursor: "pointer",
+          display: "flex",
+          justifyContent: "space-between",
+          width: 300,
+          marginBottom: "1vh"
+        }}
+      >
+        <input value={this.state.title} onChange={this.onChange} />
+        <button onClick={this.onClickSave}>save</button>
+      </li>
+    );
+  }
+
+  render() {
+    const {
+      completed,
+      id,
+      title,
+      handleChecked,
+      handleDelete,
+      handleEdit
+    } = this.props;
+    return this.state.editing
+      ? this.renderEdit(completed, id, handleEdit)
+      : this.renderItem(completed, id, title, handleChecked, handleDelete);
+  }
 }
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -5,87 +5,86 @@ import { TodoItem } from "./TodoItem";
 // Homework 3: fetch data from https://jsonplaceholder.typicode.com/todos
 
 export class TodoList extends React.Component {
-    constructor() {
-        super();
-        // Step 1: remove the static data from the state
-        this.state = {
-            items: [
-                {
-                    title: "buy things",
-                    completed: false
-                },
-                {
-                    title: "learn react",
-                    completed: true
-                }
-            ]
-        };
-    }
-    
-    // Step 2: use componentDidMount to fetch the data from the url https://jsonplaceholder.typicode.com/todos
-    // Step 3: set the data fetched from the url in the state with the this.setState function
-    // Step 4: style the todo list with some css, we will decide which one has the best design!
-
-    onAddItem = title => {
-        let items = this.state.items;
-        items.push({
-            title,
-            completed: false
-        });
-        this.setState({ items });
+  constructor() {
+    super();
+    // Step 1: remove the static data from the state
+    this.state = {
+      items: [{}]
     };
+  }
 
-    handleChecked = index => {
-        let item = this.state.items[index];
-        item.completed = !item.completed;
-        let newItems = this.state.items;
-        newItems[index] = item;
-        this.setState({
-            items: newItems
-        });
-    };
+  componentDidMount = () => {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ items: data });
+      });
+  };
 
-    handleDelete = index => {
-        let items = this.state.items;
-        const newItems = [...items.slice(0, index), ...items.slice(index + 1)];
-        // const newItems2 = this.state.items.filter((item, indexItem) => {             // other way to delete an item from an array
-        //     return indexItem !== index;
-        // });
-        this.setState({
-            items: newItems
-        });
-    };
+  // Step 2: use componentDidMount to fetch the data from the url https://jsonplaceholder.typicode.com/todos
+  // Step 3: set the data fetched from the url in the state with the this.setState function
+  // Step 4: style the todo list with some css, we will decide which one has the best design!
 
-    handleEdit = (index, title) => {
-        let items = this.state.items;
-        let item = items[index];
-        item.title = title;
-        items[index] = item;
-        this.setState({
-            items
-        });
-    };
+  onAddItem = title => {
+    let items = this.state.items;
+    items.push({
+      title,
+      completed: false
+    });
+    this.setState({ items });
+  };
 
-    render() {
-        return (
-            <div>
-                <AddItem onAddItem={this.onAddItem} />
-                <ul>
-                    {this.state.items.map((value, index) => {
-                        return (
-                            <TodoItem
-                                handleChecked={this.handleChecked}
-                                handleDelete={this.handleDelete}
-                                handleEdit={this.handleEdit}
-                                title={value.title}
-                                completed={value.completed}
-                                id={index}
-                                key={index} // you can't use it. Just used by map to optimize the rendering
-                            />
-                        );
-                    })}
-                </ul>
-            </div>
-        );
-    }
+  handleChecked = index => {
+    let item = this.state.items[index];
+    item.completed = !item.completed;
+    let newItems = this.state.items;
+    newItems[index] = item;
+    this.setState({
+      items: newItems
+    });
+  };
+
+  handleDelete = index => {
+    let items = this.state.items;
+    const newItems = [...items.slice(0, index), ...items.slice(index + 1)];
+    // const newItems2 = this.state.items.filter((item, indexItem) => {             // other way to delete an item from an array
+    //     return indexItem !== index;
+    // });
+    this.setState({
+      items: newItems
+    });
+  };
+
+  handleEdit = (index, title) => {
+    let items = this.state.items;
+    let item = items[index];
+    item.title = title;
+    items[index] = item;
+    this.setState({
+      items
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <AddItem onAddItem={this.onAddItem} />
+        <ul>
+          {this.state.items.map((value, index) => {
+            return (
+              <TodoItem
+                handleChecked={this.handleChecked}
+                handleDelete={this.handleDelete}
+                handleEdit={this.handleEdit}
+                title={value.title}
+                completed={value.completed}
+                id={index}
+                key={index} // you can't use it. Just used by map to optimize the rendering
+              />
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
 }
